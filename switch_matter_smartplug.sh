3c734f623ae7cdafda50306c0f2f046ddcdacad3
iamtanmay <iamtanmay>
2026-05-18 02:32:07 +0200

    Smart plug fix

--- switch_matter_smartplug.sh
+++ switch_matter_smartplug.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-alias matter='(node ~/matter/node_modules/@matter/nodejs-shell/dist/esm/app.js)'
+matter() {
+    node ~/matter/node_modules/@matter/nodejs-shell/dist/esm/app.js "$@"
+}
 
 if [[ " $1 " == " 0 " ]]; then
-	{ echo "commands onoff on 3 1"; sleep 15; } | matter
+    { echo "commands onoff off 3 1"; sleep 20; } | matter
 fi
+
 if [[ " $1 " == " 1 " ]]; then
-	{ echo "commands onoff off 3 1"; sleep 15; } | matter
+    { echo "commands onoff on 3 1"; sleep 20; } | matter
 fi
+
